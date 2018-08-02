81ed91873b49ead4933ee9d5c2e810d7691ac038
Xavier Salazar <salazar.xavier26@gmail.com>
2018-08-02 16:39:11 -0500

    command line tic tac toe game

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,4 +1,166 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class TicTacToe {
+    public String[][] board = {
+            {"   ", "  A  ", "  B  ", "  C  "},
+            {" 1 ", " --- ", " --- ", " --- "},
+            {" 2 ", " --- ", " --- ", " --- "},
+            {" 3 ", " --- ", " --- ", " --- "}
+    };
+    public int maxTurns = 9;
+    public ArrayList<String> currentMoves = new ArrayList<>();
+    private String playerOneName;
+    private String playerTwoName;
+    private String move;
+    private final String[] acceptableMoves = {"1a", "1b", "1c", "2a", "2b", "2c", "3a", "3b", "3c"};
+    private final String winO = " -0-  -0-  -0- ";
+    private final String winX = " -X-  -X-  -X- ";
+
+    public String getPlayerOneName() {
+        return playerOneName;
+    }
+
+    public void setPlayerOneName(String playerOneName) {
+        this.playerOneName = playerOneName;
+    }
+
+    public String getPlayerTwoName() {
+        return playerTwoName;
+    }
+
+    public void setPlayerTwoName(String playerTwoName) {
+        this.playerTwoName = playerTwoName;
+    }
+
+    public void setMove(String move) {
+        this.move = move;
+    }
+
+    public String getMove() {
+        return move;
+    }
+
+    public void displayBoard() {
+        for (String[] row : this.board) {
+            for (String col : row) {
+                System.out.print(col);
+            }
+            System.out.println();
+        }
+    }
+
+    public boolean isAcceptable(String move) {
+        boolean flag = true;
+        for (int i = 0; i < acceptableMoves.length; i++) {
+            if (move.equals(acceptableMoves[i])) {
+                flag = false;
+            }
+        }
+        return flag;
+    }
+
+    public boolean isAMoveAlreadyMade(String move) {
+        boolean flag = false;
+        if (currentMoves.contains(move)) {
+            flag = true;
+        }
+        return flag;
+    }
+
+    public void setMoveOnBoard(String move, String player) {
+        if (player.equals(this.playerOneName)) {
+            player = " -0- ";
+        } else {
+            player = " -X- ";
+        }
+
+        currentMoves.add(move);
+        switch (move) {
+            case "1a":
+                board[1][1] = player;
+                break;
+
+            case "1b":
+                board[1][2] = player;
+                break;
+
+            case "1c":
+                board[1][3] = player;
+                break;
+
+            case "2a":
+                board[2][1] = player;
+                break;
+
+            case "2b":
+                board[2][2] = player;
+                break;
+
+            case "2c":
+                board[2][3] = player;
+                break;
+
+            case "3a":
+                board[3][1] = player;
+                break;
+
+            case "3b":
+                board[3][2] = player;
+                break;
+
+            case "3c":
+                board[3][3] = player;
+                break;
+        }
+    }
+
+    public boolean checkForWin() {
+        HashMap<String, String> winningMoves = new HashMap<>();
+        winningMoves.put("top", board[1][1]+board[1][2]+board[1][3]);
+        winningMoves.put("right", board[1][3]+board[2][3]+board[3][3]);
+        winningMoves.put("bottom", board[3][1]+board[3][2]+board[3][3]);
+        winningMoves.put("left", board[1][1]+board[2][1]+board[3][1]);
+        winningMoves.put("middle", board[1][2]+board[2][2]+board[3][2]);
+        winningMoves.put("xLeftToRight", board[1][1]+board[2][2]+board[3][3]);
+        winningMoves.put("xRightToLeft", board[1][3]+board[2][2]+board[3][1]);
+
+//        String[][] winnableMoves = {
+//                {board[1][1]+board[1][2]+board[1][3]},
+//                {board[1][3]+board[2][3]+board[3][3]},
+//                {board[3][1]+board[3][2]+board[3][3]},
+//                {board[1][1]+board[2][1]+board[3][1]},
+//                {board[1][2]+board[2][2]+board[3][2]},
+//                {board[1][1]+board[2][2]+board[3][3]},
+//                {board[1][3]+board[2][2]+board[3][1]}
+//        };
+
+//        for (String[] row : winnableMoves) {
+//            for (String winningLine : row) {
+//                if (winningLine.equals(this.winO)) {
+//                    if (winningMoves.containsValue(this.winO)) {
+//                        System.out.println("win O");
+//                    }
+//                    System.out.printf("\n|======%s is the WINNER!======|\n", this.playerOneName);
+//                    return true;
+//                } else if (winningLine.equals(this.winX)) {
+//                    if (winningMoves.containsValue(this.winX)) {
+//                        System.out.println("win X");
+//                    }
+//                    System.out.printf("\n|======%s is the WINNER!======|\n", this.playerTwoName);
+//                    return true;
+//                }
+//            }
+//        }
+//
+        if (winningMoves.containsValue(this.winO)) {
+            System.out.printf("\n|======%s is the WINNER!======|\n", this.playerOneName);
+            return true;
+        } else if (winningMoves.containsValue(this.winX)) {
+            System.out.printf("\n|======%s is the WINNER!======|\n", this.playerTwoName);
+            return true;
+        }
+
+        return false;
+    }
 }
--- src/TicTacToeTest.java
+++ src/TicTacToeTest.java
@@ -1,4 +1,70 @@
-package PACKAGE_NAME;
+import javax.print.DocFlavor;
+import java.util.Scanner;
 
 public class TicTacToeTest {
+
+    static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        TicTacToe game = new TicTacToe();
+
+        System.out.print("Player one enter your name: ");
+        game.setPlayerOneName(input.nextLine());
+        System.out.print("Player two enter your name: ");
+        game.setPlayerTwoName(input.nextLine());
+        System.out.printf("[%s] You are 0's\n", game.getPlayerOneName());
+        System.out.printf("[%s] You are X's\n\n", game.getPlayerTwoName());
+        game.displayBoard();
+
+        while (game.maxTurns >= 0) {
+            // --- Player One ---
+            System.out.printf("[%s] Enter your move: ", game.getPlayerOneName());
+            game.setMove(input.nextLine());
+            checkForAcceptableMoves(game);
+            game.setMoveOnBoard(game.getMove(), game.getPlayerOneName());
+            game.displayBoard();
+            if (game.checkForWin()) {
+                break;
+            }
+            game.maxTurns--;
+            // --- Player One ---
+
+            // Last turn becomes 0 here
+            if (game.maxTurns == 0) {
+                System.out.println("Nobody wins.");
+                break;
+            }
+
+            // --- Player Two ---
+            System.out.printf("[%s] Enter your move: ", game.getPlayerTwoName());
+            game.setMove(input.nextLine());
+            checkForAcceptableMoves(game);
+            game.setMoveOnBoard(game.getMove(), game.getPlayerTwoName());
+            game.displayBoard();
+            if (game.checkForWin()) {
+                break;
+            }
+            game.maxTurns--;
+            // --- Player Two ---
+        }
+    }
+
+    // Check for move that is acceptable and move that has already been made
+    public static String checkForAcceptableMoves(TicTacToe game) {
+        if (game.isAcceptable(game.getMove())) {
+            while (game.isAcceptable(game.getMove())) {
+                System.out.println("Not an acceptable move.");
+                System.out.print("Please enter another: ");
+                game.setMove(input.nextLine());
+            }
+        }
+        if (game.isAMoveAlreadyMade(game.getMove())) {
+            while (game.isAMoveAlreadyMade(game.getMove())) {
+                System.out.printf("The move %s has already been made\n", game.getMove());
+                System.out.print("Enter another move: ");
+                game.setMove(input.nextLine());
+            }
+        }
+        return game.getMove();
+    }
 }
